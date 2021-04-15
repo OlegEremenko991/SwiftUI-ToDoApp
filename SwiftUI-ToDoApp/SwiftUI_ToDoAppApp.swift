d7e04f7928909b1f8be8893b72f8c3941920720f
Oleg991 <o.n.eremenko@gmail.com>
2021-04-15 09:47:01 +0400

    Initial Commit

---
+++ SwiftUI-ToDoApp/SwiftUI_ToDoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI_ToDoAppApp.swift
+//  SwiftUI-ToDoApp
+//
+//  Created by Олег Еременко on 15.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToDoAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
